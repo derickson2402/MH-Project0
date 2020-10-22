5b848fb699acdfc98d9f4de75e79634b81817f15
Dan Erickson <derickson2402@gmail.com>
2020-10-22 00:24:35 -0400

    Initialize repo, add project outline and script

---
+++ passwordHackScript.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python
+
+# This script is created by Dan Erickson for a Michigan Hackers project
+
+# The script generates a .txt file with a list of potential passwords     \
+# that fulfill the parameters set in the Project 0: Forgot Your Password? \
+# .pdf file included in this repository.
